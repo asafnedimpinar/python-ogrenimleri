37a524ca057724d0d83b2b1cb66ae96e887a8417
Asaf Nedim Pınar <94077369+147399@users.noreply.github.com>
2024-03-16 12:35:27 +0300

    Add files via upload

---
+++ 32 turtle bilgileri.py
@@ -0,0 +1,22 @@
+import turtle  # turtle modülünü içe aktar
+
+turtle.speed(1)  # turtle'ın hızını ayarla
+turtle.goto(100, 200)  # (100, 200) koordinatlarına git
+turtle.forward(100)  # İleri git (100 birim)
+
+turtle.fd(5)  # İleri git (5 birim)
+turtle.shape('turtle')  # Şekli turtle olarak ayarla
+turtle.left(90)  # Sola dön (90 derece)
+turtle.color('red')  # Rengi kırmızı yap
+turtle.backward(230)  # Geri git (230 birim)
+turtle.shape('circle')  # Şekli daire olarak ayarla
+turtle.speed(1)  # turtle'ın hızını ayarla
+turtle.right(90)  # Sağa dön (90 derece)
+turtle.hideturtle()  # Turtle'ı gizle
+turtle.shape('arrow')  # Şekli ok olarak ayarla
+turtle.bk(160)  # Geri git (160 birim)
+turtle.goto(-100, -50)  # (-100, -50) koordinatlarına git
+turtle.showturtle()  # Turtle'ı göster
+turtle.forward(100)  # İleri git (100 birim)
+
+turtle.mainloop()  # Turtle penceresini açık tut
