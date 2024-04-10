d5027eddf9bb7e9db51fbc0a84008697e97baaf5
Asaf Nedim Pınar <94077369+147399@users.noreply.github.com>
2024-04-10 14:10:10 +0300

    Update 1  değisken turleri.py

--- 1  değisken turleri.py
+++ 1  değisken turleri.py
@@ -1,24 +1,30 @@
-print(3+3)    # 3+3 toplatıldı
+# Toplamı ekrana yazdırma
+print(3 + 3)  # 3+3 toplandı
+
+# 3'ün tipini ekrana yazdırma
 print(type(3))
-print(type(3.4))  #  cevabın tıpını yazdı
-print( 7000 + (7000*0.18))
 
-urun1=7000
-urun2=8600
-kdv=0.18
-print(urun1 + (urun1*kdv)) 
-print(urun2 + (urun2*kdv))
+# 3.4'ün tipini ekrana yazdırma
+print(type(3.4))  # Cevabın tipini yazdı
 
+# KDV hesaplama
+print(7000 + (7000 * 0.18))
+
+# Ürün fiyatları
+urun1 = 7000
+urun2 = 8600
+kdv = 0.18
+
+# Ürünlerin KDV'li fiyatlarını hesaplama ve ekrana yazdırma
+print(urun1 + (urun1 * kdv)) 
+print(urun2 + (urun2 * kdv))
+
+# İsim değişkeninin tipini ekrana yazdırma
 name = "levent"
 print(type(name))
 
-ısStudent =True
-print(type(ısStudent))
- 
-
-    
-
-
-
+# Öğrenci olup olmadığını belirten değişkenin tipini ekrana yazdırma
+isStudent = True
+print(type(isStudent))
 
 
