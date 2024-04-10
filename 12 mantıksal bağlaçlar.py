70487f99d301bef846fb539d43b0e0473165aada
Asaf Nedim Pınar <94077369+147399@users.noreply.github.com>
2024-04-10 14:12:01 +0300

    Update 12 mantıksal bağlaçlar.py

--- 12 mantıksal bağlaçlar.py
+++ 12 mantıksal bağlaçlar.py
@@ -1,8 +1,17 @@
 # and baglacı dogru dogru olursa true degirisi false
 # or baglacı yanlıs yanlıs olursa false gerisi true olur
+# Kullanıcı bilgilerini içeren liste
+kullanıcıbilgileri = ["asdas98", "1234567k"]
 
-kullanıcıbilgileri= ["asdas98","1234567k"]
-kadi = input("kullanıcı adını giriniz :")
-sifre= input("şifreyi giriniz :")
+# Kullanıcıdan kullanıcı adını girmesini isteme
+kadi = input("Kullanıcı adını giriniz: ")
+
+# Kullanıcıdan şifreyi girmesini isteme
+sifre = input("Şifreyi giriniz: ")
+
+# Kullanıcının girdiği kullanıcı adı ve şifrenin doğruluğunu kontrol etme
+# Eğer kullanıcı adı ve şifre listedeki bilgilerle uyuşuyorsa 'True', aksi takdirde 'False' değeri atanır
 a = kullanıcıbilgileri[0] == kadi and kullanıcıbilgileri[1] == sifre
+
+# Sonucu ekrana yazdırma
 print(a)
