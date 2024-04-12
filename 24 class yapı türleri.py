5b8be777f64121cc0a98ac77faa9a974e372708b
Asaf Nedim Pınar <94077369+147399@users.noreply.github.com>
2024-04-12 17:10:38 +0300

    Update 24 class yapı türleri.py

--- 24 class yapı türleri.py
+++ 24 class yapı türleri.py
@@ -1,32 +1,39 @@
-"""
-
-class araba():
-    def __init__(self,marka,model,fiyat,renk) :
-        self.gelenmarka : marka
-        self.gelenmodel : model
-        self.gelenfiyat : fiyat
-        self.gelenrenk : renk
-
-
-    def bilgileriyazdır(self):
-        print(self.gelenmarka,self.gelenmodel,self.gelenfiyat,self.gelenrenk)
-
-araba1 = araba("bmw","i30",30_000,"mavi")
-araba2 = araba("mercedes","e5",50_000,"siyah")
-
-
-araba1.bilgileriyazdır()
-araba2.bilgileriyazdır()
-"""
-
 
 class Araba():
-    renk = "kirmizi"
-    model= "Clio"
-    fiyat = 50_000
-    marka ="renault"
-    
-    
-    
-    def Yaz(self):
-        print(self.renk,self.model,self.fiyat,self.marka)
+    """
+    Araba sınıfı, araba nesnelerinin özelliklerini ve davranışlarını tanımlar.
+    """
+
+    def __init__(self, marka, model, fiyat, renk):
+        """
+        Araba sınıfının başlatıcı yöntemi. Yeni bir araba nesnesi oluşturur.
+
+        Parametreler:
+            marka (str): Arabanın markası.
+            model (str): Arabanın modeli.
+            fiyat (int): Arabanın fiyatı.
+            renk (str): Arabanın rengi.
+        """
+        self.gelenmarka = marka  # Arabanın markası
+        self.gelenmodel = model  # Arabanın modeli
+        self.gelenfiyat = fiyat  # Arabanın fiyatı
+        self.gelenrenk = renk    # Arabanın rengi
+
+    def bilgileriyazdır(self):
+        """
+        Arabanın bilgilerini ekrana yazdıran metot.
+        """
+        print(self.gelenmarka, self.gelenmodel, self.gelenfiyat, self.gelenrenk)
+
+
+# Araba nesneleri oluşturma
+araba1 = Araba("bmw", "i30", 30_000, "mavi")
+araba2 = Araba("mercedes", "e5", 50_000, "siyah")
+
+# Araba bilgilerini yazdırma
+araba1.bilgileriyazdır()
+araba2.bilgileriyazdır()
+Bu şekilde, her bir metodu ve parametreyi neyin temsil ettiğini açıklayan açıklamalar eklenmiştir.
+
+
+
